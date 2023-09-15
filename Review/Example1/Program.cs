a38a969eb5e63d6fb000ba25eca9ffa271f51e1d
Snozm <andreypetrov246@gmail.com>
2023-09-15 09:40:34 +0300

    created first oop task class

---
+++ Review/Example1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] names = { "Andrey", "Vladi", "Martin", "Pavel", "Boris" };
+
+        Student[] students = new Student[names.Length];
+
+        Random x = new Random();
+
+        for(int i = 0; i < students.Length; i++)
+        {
+            students[i] = new Student(names[i], 1, x.Next(0, 101));
+        }
+    }
+}
+
+public class Student{ 
+
+    private string name;
+    private int section;
+    private int finalGrade;
+    public Student(string name, int section, int finalGrade)
+    {
+        this.name = name;
+        this.section = section;
+        this.finalGrade = finalGrade;
+    }
+
+    public string evaluate()
+    {
+        string group;
+
+        if(finalGrade <= 35)
+        {
+            group = "poor";
+        }
+        else if(finalGrade <= 50)
+        {
+            group = "satisfactory";
+        }
+        else if (finalGrade <= 70)
+        {
+            group = "good";
+        }
+        else if (finalGrade <= 85)
+        {
+            group = "very good";
+        }
+        else if (finalGrade <= 95)
+        {
+            group = "excellent";
+        }
+        else
+        {
+            group = "exceed expectations";
+        }
+        return $"I am {name} and my groups is {group}";
+    }
+
+}
