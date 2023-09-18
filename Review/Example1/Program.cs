4d9a2a920b2a5b30eae9918e0368231efdc733b6
Snozm <andreypetrov246@gmail.com>
2023-09-18 13:06:55 +0300

    ????

--- Review/Example1/Program.cs
+++ Review/Example1/Program.cs
@@ -12,8 +12,17 @@ public class Program
 
         for(int i = 0; i < students.Length; i++)
         {
-            students[i] = new Student(names[i], 1, x.Next(0, 101));
+            if(i%2 == 0)
+            {
+                students[i] = new Student(names[i], 1, x.Next(0, 101));
+            }
+            else
+            {
+                students[i] = new Student(names[i], 1);
+            }
         }
+
+        Console.WriteLine(students[2].evaluate());
     }
 }
 
@@ -22,42 +31,53 @@ public class Student{
     private string name;
     private int section;
     private int finalGrade;
+    private static List<int> allGrades = new List<int>();
     public Student(string name, int section, int finalGrade)
     {
         this.name = name;
         this.section = section;
         this.finalGrade = finalGrade;
+        allGrades.Add(finalGrade);
+    }
+    public Student(string name, int section)
+    {
+        this.name = name;
+        this.section = section;
     }
 
     public string evaluate()
     {
-        string group;
+        if (finalGrade != 0)
+        {
+            string group;
 
-        if(finalGrade <= 35)
-        {
-            group = "poor";
+            if (finalGrade <= 35)
+            {
+                group = "poor";
+            }
+            else if (finalGrade <= 50)
+            {
+                group = "satisfactory";
+            }
+            else if (finalGrade <= 70)
+            {
+                group = "good";
+            }
+            else if (finalGrade <= 85)
+            {
+                group = "very good";
+            }
+            else if (finalGrade <= 95)
+            {
+                group = "excellent";
+            }
+            else
+            {
+                group = "exceed expectations";
+            }
+            return $"I am {name} and my group is {group}. The average of all grades is {allGrades.Average()}.";
         }
-        else if(finalGrade <= 50)
-        {
-            group = "satisfactory";
-        }
-        else if (finalGrade <= 70)
-        {
-            group = "good";
-        }
-        else if (finalGrade <= 85)
-        {
-            group = "very good";
-        }
-        else if (finalGrade <= 95)
-        {
-            group = "excellent";
-        }
-        else
-        {
-            group = "exceed expectations";
-        }
-        return $"I am {name} and my groups is {group}";
+        return $"I am {name} and I haven't taken the test";
     }
 
 }
