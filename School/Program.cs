7ccd6c51566897cce243e9ccecbb2e63d4132cd7
Snozm <andreypetrov246@gmail.com>
2023-10-18 15:31:39 +0300

    school task

--- School/Program.cs
+++ School/Program.cs
@@ -5,12 +5,33 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        School acs = new School();
+        Dictionary<string, School> schools = new Dictionary<string, School>();
+
+        schools.Add("ACS", new School());
         string[] courses = { "math", "physics", "informatics" };
-        acs.AddStudent("Andrey", courses);
-        foreach(string course in acs.GetCourses("Andrey"))
+        schools["ACS"].AddStudent("Andrey", courses);
+        schools["ACS"].AddStudent("Vladi", courses);
+        courses[2] = "IT";
+        schools["ACS"].AddStudent("Martin", courses);
+
+        schools.Add("SMG", new School());
+        string[] smgCourses = { "math", "physics", "informatics", "art" };
+        schools["SMG"].AddStudent("Daniel", smgCourses);
+
+        foreach (KeyValuePair<string, School> school in schools)
         {
-            Console.WriteLine(course);
+            Console.WriteLine($"Name of school: {school.Key}");
+            foreach(KeyValuePair<string, string[]> student in school.Value.GetStudents())
+            {
+                Console.WriteLine($"Student name: {student.Key}");
+                Console.WriteLine("Courses taken: ");
+                foreach(string course in student.Value)
+                {
+                    Console.Write($"{course} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
@@ -25,6 +46,10 @@ public class School
     public void AddStudent(string name, string[] courses) 
     {
         students.Add(name, courses);
+    }
+    public Dictionary<string, string[]> GetStudents()
+    {
+        return students;
     }
     public string[] GetCourses(string name)
     {
